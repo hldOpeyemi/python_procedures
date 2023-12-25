a51870a88dc29b874967ad676a6d3704843290f5
Opeyemi <yemi.otu@outlook.com>
2023-12-25 11:26:41 -0700

    get item by input

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import mariadb
 import dbcreds
 
-
 conn = mariadb.connect(
     user=dbcreds.user, 
     password=dbcreds.password,
@@ -11,7 +10,31 @@ conn = mariadb.connect(
    )
 cursor = conn.cursor()
 cursor.execute('CALL return_all_items()')
-result = cursor.fetchall()
 
-print (result)
+result = cursor.fetchall() 
+
+items = result
+
+# print(items)
    
+cursor.close
+conn.close
+
+
+# for i in range(len(items)):
+#     # print(items[i][0:2])
+
+user_input = input('Please enter a price: ')
+user_input = int(user_input)
+# print(user_price)
+
+cursor = conn.cursor()
+cursor.execute('CALL get_item_by_user_input(?)',[user_input])
+
+cursor.close
+conn.close
+
+result = cursor.fetchall() 
+print(result)
+
+
